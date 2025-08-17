6a869acc2193ebe1162a22476123ffad0c5157c2
nn-gthub <nairneena98@gmail.com>
2025-08-18 00:45:26 +0530

    Add files via upload

---
+++ HR DATA CLEANING.sql
@@ -0,0 +1,61 @@
+select *
+from hr;
+
+alter table hr
+change column ï»¿id emp_id varchar(20) null
+
+describe hr;
+
+select birthdate from hr;
+
+SET sql_safe_updates = 0;
+
+update hr
+set birthdate = case
+	WHEN birthdate LIKE '%/%' THEN date_format(str_to_date(birthdate, '%m/%d/%Y'), '%Y-%m-%d')
+    WHEN birthdate LIKE '%-%' THEN date_format(str_to_date(birthdate, '%m-%d-%Y'), '%Y-%m-%d')
+    ELSE NULL
+end;
+
+ALTER TABLE hr
+MODIFY COLUMN birthdate DATE;
+
+update hr
+set hire_date = case
+	WHEN hire_date LIKE '%/%' THEN date_format(str_to_date(hire_date, '%m/%d/%Y'), '%Y-%m-%d')
+    WHEN hire_date LIKE '%-%' THEN date_format(str_to_date(hire_date, '%m-%d-%Y'), '%Y-%m-%d')
+    ELSE NULL
+end;
+
+ALTER TABLE hr
+MODIFY COLUMN hire_date DATE;
+
+select termdate from hr;
+
+UPDATE hr
+SET termdate = IF(termdate IS NOT NULL AND termdate != '', date(str_to_date(termdate, '%Y-%m-%d %H:%i:%s UTC')), '0000-00-00')
+WHERE true;
+
+SET sql_mode = 'ALLOW_INVALID_DATES';
+ALTER TABLE hr
+MODIFY COLUMN termdate DATE;
+
+ALTER TABLE hr ADD COLUMN age INT;
+UPDATE hr
+SET age = timestampdiff(YEAR, birthdate, CURDATE());
+
+SELECT 
+	min(age) AS youngest,
+    max(age) AS oldest
+FROM hr;
+
+SELECT count(*) FROM hr WHERE age < 18;
+
+SELECT COUNT(*) FROM hr WHERE termdate > CURDATE();
+
+SELECT COUNT(*)
+FROM hr
+WHERE termdate = '0000-00-00';
+
+
+
